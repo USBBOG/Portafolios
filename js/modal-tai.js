5b2214329c12f38fad1cb0e8948a8f2eef969a05
DeivTor <deiver.torres168@gmail.com>
2024-11-12 11:48:05 -0500

    Funcionalidad de los Modal para TAI

--- js/modal-tai.js
+++ js/modal-tai.js
@@ -9,16 +9,24 @@ document.addEventListener("DOMContentLoaded", function(){
             modal6: "https://www.youtube.com/embed/3JvkqC3mMDg?si=QzhTAulld2nKv6N8"
         },
         { // { TAI 2 }
-
+            modal1: "https://www.youtube.com/embed/1FvusBCVfvs?si=uUjjKrg4iTkrcLpG",
+            modal2: "https://www.youtube.com/embed/22q6pZJb5KE?si=qBCR02LxGOcj7d3j",
+            modal3: "https://drive.google.com/file/d/10DS63ysPgO_XMaO0jsqdMobsIB4IzBag/preview",
+            modal4: "https://www.youtube.com/embed/hHfB8FRM_fo?si=rldn0WnBl7t8zCR2",
+            modal5: "https://www.youtube.com/embed/8t41IBHIpXo?si=JSse_9UYXLRYEKm5",
+            modal6: "https://www.youtube.com/embed/9l2pWynFar8?si=Me8o8B_UZU06FU8P"
         },
         { // { TAI 3 }
-
+            modal1: "https://www.youtube.com/embed/XjF6AMd-RJw?si=YCohWLpGIYP9LjrP",
+            modal2: "https://www.youtube.com/embed/5K_6LHrPRVk?si=I_lMSUl83cmi0WXB",
+            modal3: "https://www.youtube.com/embed/sPXcqsjQpJo?si=rVzGD9ttsrQMde5j"
         },
         { // { TAI 4 }
-
+            modal1: "https://www.youtube.com/embed/z3laB54ggFk?si=RlG-WYKVBS28elYC"
         },
         { // { TAI 5 }
-
+            modal1: "https://academiausbbogedu-my.sharepoint.com/personal/lcmarquezs_academia_usbbog_edu_co/_layouts/15/embed.aspx?UniqueId=77840d7c-9dfd-488d-b1c1-49a7e87c0ce9&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create",
+            modal2: "https://www.youtube.com/embed/ASJ6wSzMdZY?si=DrMNP5E0cJw8aCY1"
         },
         { // { TAI 6 }
             
@@ -47,6 +55,9 @@ document.addEventListener("DOMContentLoaded", function(){
         const url = youtubeModals[identifyWindow()][modalId];
         
         if (container && url) {
+            if(url.includes("drive")){
+                container.innerHTML = `<iframe src="${url}" width="640" height="480" allow="autoplay"></iframe>`;
+            }
             container.innerHTML = `<iframe width="560" height="315" src="${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
         }
     }
