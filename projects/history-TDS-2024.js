eb53a94636b3addcb7a52913dd37f0b60ec716d0
mike-ki17 <michael16cd@gmail.com>
2025-04-09 12:24:47 -0500

    data base history projects

---
+++ projects/history-TDS-2024.js
@@ -0,0 +1,93 @@
+const projects = [
+  {
+    nombre: "What's New Games",
+    descripcion:
+      "En nuestra plataforma, tenemos una misión clara y es ayudar a los freelancers a encontrar trabajos sencillos y específicos ofrecidos por los clientes, permitiéndoles tomarlo como una oportunidad laboral o un pasatiempo. Al mismo tiempo, nos esforzamos por ayudar a los clientes a resolver sus problemas específicos de manera eficiente.",
+    portafolio: "https://jeancarloguzmanc.github.io/WhatsNewGames-Repository/",
+    logo: "assets/tds/s6/3.png",
+  },
+  {
+    nombre: "RoverCrop",
+    descripcion:
+      "El proyecto 'RoverCrop' se enfoca en mejorar la producción agrícola mediante un simulador que analiza el comportamiento de robots agricultores en diferentes tipos de terrenos.",
+    portafolio: "https://deivtor.github.io/RoverCrop-Page/",
+    logo: "assets/tds/s6/4.png",
+  },
+  {
+    nombre: "LDS-C",
+    descripcion:
+      "Este proyecto se enfoca en solucionar uno de los problemas más comunes que enfrentan las personas sordas en su interacción con las entidades públicas: la falta de una comunicación efectiva. En muchas ocasiones, las personas sordas encuentran dificultades para expresar sus necesidades y comprender las respuestas de los funcionarios públicos debido a la falta de capacitación en lenguaje de señas y otras herramientas de comunicación.",
+    portafolio: "https://anyi404.github.io/ProyectoLDSfinal/",
+    logo: "assets/tds/s6/5.png",
+  },
+  {
+    nombre: "DNQ Motor",
+    descripcion:
+      "Desarrollar una plataforma de comercio electrónico especializada en la venta de repuestos para motos, con el propósito de ofrecer a los clientes una amplia gama de productos de alta calidad, una experiencia de compra segura y conveniente, y un servicio al cliente excepcional, con el fin de convertirse en un referente confiable en el mercado de repuestos para motos en línea.",
+    portafolio: "https://jfquinch.github.io/DNQ-Motor-Portafolio/#footer",
+    logo: "assets/tds/s6/6.png",
+  },
+  {
+    nombre: "VSTR",
+    descripcion:
+      "Vstr es una herramienta para la creación, edición, almacenamiento y transmisión de videos, diseñada para facilitar el proyecto de producción de contenido audiovisual y ayudarte a llegar a tu audiencia de manera efectiva en tus plataformas preferidas.",
+    portafolio: "https://project-vstr-herramienta-web.vercel.app/",
+    logo: "assets/tds/s6/7.png",
+  },
+  {
+    nombre: "ADOC",
+    descripcion:
+      "En el marco de nuestro proyecto 'ADOC Tú Gestor Documental' haremos uso de Figma para diseñar los mockUps de identidad de nuestro gestor, colores, tipo de letra, logo, botones, etc. ReactJS como lenguaje para el frontend, Laravel como framework de php será el responsable de la lógica de negocio y MariaDB será la base de datos donde persistirá la información y correlación de roles de los usuarios de nuestra plataforma.",
+    portafolio: "https://michaelbogota.github.io/adoc-page/",
+    logo: "assets/tds/s6/8.png",
+  },
+  {
+    nombre: "Programmasy",
+    descripcion:
+      "En esencia programmasy es un programa web que se enfoca en ayudarte en tu formacion digital, lo que hace programmasy, es reunir todos los recursos de calidad existentes en el internet, ya se en cursos, libros, blogs, comunidades, videos, etc. Tambien te da la posiblidad de adquirir conocimiento en bases de datos, frameworks, arquitecturas de software, todo lo que debes de tener de manera directa para tus conocimientos y habilidades.",
+    portafolio:
+      "https://github.com/RubenAlexis663/PROGFINAL/wiki/Documentaci%C3%B3n",
+    logo: "assets/tds/s6/9.png",
+  },
+  {
+    nombre: "PIN UP",
+    descripcion:
+      "PINUP is the integrative project developed by the team of 5 members as part of the requirements for the completion of our studies at the San Buenaventura University. PINUP aims to provide a comprehensive solution for the virtualization of the PINUP dance academy located in the city of Medellín, facilitating the management of its users, classes, students, teachers among other utilities.",
+    portafolio: "https://pinup-7bs6.onrender.com/",
+    logo: "assets/tds/s6/10.png",
+  },
+  {
+    nombre: "Vive Explorando",
+    descripcion:
+      "No se encontro ninguna descripcion y repositorio acerca del correspondiente proyecto.",
+    portafolio: "https://github.com/Brayandev8/Vive_Explorando.github.io",
+    logo: "assets/tds/s6/11.png",
+  },
+  {
+    nombre: "Guia de suplementos deportivos",
+    descripcion:
+      "El presente proyecto tiene como finalidad desarrollar una aplicación móvil que guíe al consumidor de suplementos deportivos en la compra de un producto específico por medio de un conjunto de herramientas de búsqueda y filtrado de información categorizada y una interfaz gráfica intuitiva. La realización de este proyecto es debido a la falta de aplicaciones competentes o dedicadas a la suplementación deportiva en el mercado actual.",
+    portafolio: "https://sanmonor.github.io/Proyecto_Suplementos_Deportivos/",
+    logo: "assets/tds/s6/2.png",
+  },
+  {
+    nombre: "Funny Mind",
+    descripcion:
+      "Funny Mind es un proyecto revolucionario que se enfoca en la rehabilitación cognitiva de niños con traumas craneoencefálicos, utilizando una plataforma web para ofrecer ejercicios y juegos interactivos. Este enfoque innovador busca mejorar habilidades cognitivas fundamentales, como la memoria, la atención, y las funciones ejecutivas través de una plataforma accesible y efectiva que se integra en el entorno diario de los niños.",
+    portafolio: "https://andresfelipe21.github.io/share-point/",
+    logo: "assets/tds/s6/12.png",
+  },
+  {
+    nombre: "Passt",
+    descripcion:
+      "LA POCA GESTIÓN EN EL MANEJO DE LAS CONTRASEÑAS Y DATOS SENSIBLES REPOSADOS EN LAS DIFERENTES PLATAFORMAS DE INFORMACIÓN RESALTAN LA GRAN VULNERABILIDAD QUE POSEEN LOS DATOS SI NO SE PROTEGEN PARA LO CUAL SE PRECISA DE UNA HERRAMIENTA MÓVIL QUE AYUDE EN LA GESTIÓN DE TALES DATOS SENSIBLES. EL PROYECTO INICIALMENTE SE PENSÓ A FIN DE CUBRIR UNA NECESIDAD DE PROPIA DEL EQUIPO DESARROLLADOR QUE AL IGUAL SIENTE LA NECESIDAD Y BUSCA CUBRIRLA, ACTUALMENTE EL USO DE APLICATIVOS SE HACE MÁS FRECUENTE EN NUESTRAS VIDAS E INGRESAR LOS DATOS PERSONALES EN ELLOS ES PROPIO DEL DÍA A DÍA LO QUE HACE IMPERATIVO EL EJERCER SEGURIDAD EN TODO CONTEXTO.",
+    portafolio: "https://fe1t0r.github.io/Passt.github.io/",
+    logo: "assets/tds/s6/13.png",
+  },
+  {
+    nombre: "AutisTec",
+    descripcion: "Falta descripción.",
+    portafolio: "#",
+    logo: "assets/tds/s6/14.png",
+  },
+];
